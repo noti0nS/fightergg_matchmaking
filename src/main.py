5e72862338817b87b79183a1f49fafd306bc9f60
noti0nS <joselucas0303@gmail.com>
2025-11-06 21:03:29 -0300

    feat: establish database connection and load environment variables

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
+from dotenv import load_dotenv
+from infra import db
+
 def main():
+    load_dotenv() # Carrega variáveis de ambiente do arquivo .env
+    
     print("Hello, World!")
 
 if __name__ == "__main__":
