1594644fa5f0f2169beaa868741c074674989529
umgabrie1 <contasecundariabiel2@gmail.com>
2025-11-09 19:42:55 -0300

    feat: implement register function with email and password validation

--- src/services/auth.py
+++ src/services/auth.py
@@ -1,7 +1,6 @@
 from utils import validators_utils, password_utils
 from data import user
 
-
 def login(email: str, password: str) -> bool:
     try:
         if not validators_utils.is_email_valid(email):
@@ -20,6 +19,45 @@ def login(email: str, password: str) -> bool:
         return False
     
 
-def register() -> bool:
-    # TODO Gabriel: Implementar função.
-    pass
+def register(nome_completo: str, nickname: str, email: str, password: str) -> bool:
+    #Registra um novo usuario no sistema
+    try: 
+        #Validar E-mail
+        if not validators_utils.is_email_valid(email):
+            print(f"O e-mail fornecido é inválido: {email}")
+            return False
+        
+        #Validar Formato da Senha
+        password_status = validators_utils.get_password_status(password)
+        if password_status:
+            print(f"A senha fornecida é invalida: {password_status}")
+            return False
+        
+        #Validar erros
+        if user.check_email_exists(email):
+            print("Erro: Este e-mail já está cadastrado")
+            return False
+
+        if user.check_nickname_exists(nickname):
+            print("Erro: Este e-mail já está cadastrado")
+            return False
+        
+        #Criptografia de senha
+        hashed_password = password_utils.hash_password(password)
+        success = user.create_user(
+            nome_completo=nome_completo,
+            nickname=nickname,
+            email=email,
+            senha_hash=hashed_password 
+        )
+        if success:
+            print(f"Usuário '{nickname}' registrado com sucesso!")
+            return True
+        else:
+            print("Ocorreu um erro ao registrar o usuário no banco de dados.")
+            return False
+
+    except Exception as e:
+        print(f"Um erro inesperado ocorreu durante o registro: {e}")
+        return False
+
